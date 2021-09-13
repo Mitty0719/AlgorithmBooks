b37436b58604d03491a07b86b7fd7b9373856574
Mitty0719 <sprout071900@gmail.com>
2021-09-14 06:54:19 +0900

    10172 clear

--- AlgorithmBooks/Program.cs
+++ AlgorithmBooks/Program.cs
@@ -6,7 +6,42 @@ namespace AlgorithmBooks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine(a + b);
+            //스캐너로 풀어야함000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #region 10172
+            //Console.WriteLine("|\\_/|" + "\n" +
+            //                  "|q p|   /}" + "\n" +
+            //                  "( 0 )\"\"\"\\" + "\n" +
+            //                  "|\"^\"`    |" + "\n" +
+            //                  "||_/=\\\\__|");
+            #endregion
         }
     }
 }
