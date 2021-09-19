e91ce61b9b17cde65a29755f846f4e44e8319485
Mitty0719 <sprout071900@gmail.com>
2021-09-19 21:39:42 +0900

    Clear 2588, 1330, 9498

--- AlgorithmBooks/Program.cs
+++ AlgorithmBooks/Program.cs
@@ -6,34 +6,84 @@ namespace AlgorithmBooks
     {
         static void Main(string[] args)
         {
+            #region 2753
+
+            int year = Int32.Parse(Console.ReadLine());
+            string res = "0";
+
+            if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                res = "1";
+            }
+
+            Console.WriteLine(res);
+            #endregion
+
+            #region 9498
+            //int score = Int32.Parse(Console.ReadLine());
+            //string res = "F";
+
+            //if(score >= 90)
+            //{
+            //    res = "A";
+            //}
+            //else if(score >= 80)
+            //{
+            //    res = "B";
+            //}
+            //else if(score >= 70)
+            //{
+            //    res = "C";
+            //}
+            //else if(score >= 60)
+            //{
+            //    res = "D";
+            //}
+
+            //Console.WriteLine(res);
+            #endregion
+
+            #region 1330
+            //string[] input = Console.ReadLine().Split();
+
+            //int leftNum = Int32.Parse(input[0]);
+            //int rightNum = Int32.Parse(input[1]);
+            //string res = "";
+
+
+            //if(leftNum > rightNum)
+            //{
+            //    res = ">";
+            //}
+            //else if(leftNum < rightNum)
+            //{
+            //    res = "<";
+            //}
+            //else
+            //{
+            //    res = "==";
+            //}
+
+            //Console.WriteLine(res);
+            #endregion
 
             #region 2588
 
-            long anum = Int32.Parse(Console.ReadLine());
-            long bnum = Int32.Parse(Console.ReadLine());
-            long num = 1;
-            long temp = bnum;
-            while(num > 0)
-            {
-                num = bnum % 10;
-                bnum = bnum / 10;
+            //long anum = Int32.Parse(Console.ReadLine());
+            //long bnum = Int32.Parse(Console.ReadLine());
+            //long num = 1;
+            //long temp = bnum;
+            //while(bnum > 0)
+            //{
+            //    num = bnum % 10;
+            //    bnum = bnum / 10;
 
-                if(num > 0)
-                {
-                    Console.WriteLine(anum * num);
-                }
-            }
+            //    Console.WriteLine(anum * num);
+            //}
 
-            Console.WriteLine(anum * temp);
+            //Console.WriteLine(anum * temp);
 
-
-            
             #endregion
-
-
-
-
-
 
             #region 10430
             //string[] strs = Console.ReadLine().Split(' ');
@@ -55,7 +105,6 @@ namespace AlgorithmBooks
             //Console.WriteLine(Int32.Parse(strs[0]) / Int32.Parse(strs[1]));
             //Console.WriteLine(Int32.Parse(strs[0]) % Int32.Parse(strs[1]));
             #endregion
-
 
             #region 1000, 1001, 1008, 10998
             //string[] strs = Console.ReadLine().Split();
